3150cd9d937e9ed0e2fb410d6312d2a51bebb1d0
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-02-06 17:48:17 -0500

    style.css

--- server.js
+++ server.js
@@ -22,17 +22,17 @@ const sess = {
 app.use(session(sess));
 
 //const helpers = require('./utils/helpers');
+//helpers
+const hbs = exphbs.create({  });
 
-//const hbs = exphbs.create({ helpers });
-
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
