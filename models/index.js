731163c5a3a14155ce3b1c8a1c06b5ba3c61cf2e
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-02-09 20:54:04 -0500

    fixing helper

--- models/index.js
+++ models/index.js
@@ -10,25 +10,29 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
   foreignKey: 'user_id',
 });
-
+//-------------------------------------
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
-  onDelete: 'SET NULL'
+  onDelete: 'cascade',
+  hooks:true
 });
 
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+  onDelete: 'cascade',
+  hooks:true
 });
-
+//--------------------------------------------
 User.hasMany(Comment, {
   foreignKey: 'user_id',
- 
+  onDelete: 'cascade',
+  hooks:true
 });
 
 Post.hasMany(Comment, {
   foreignKey: 'post_id',
- onDelete:'SET NULL'
+  onDelete: 'cascade',
+  hooks:true
 });
 
 module.exports = { User, Post, Comment };
