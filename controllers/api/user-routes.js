8f0166855154e46dfa24c72e94d7681e98b2638e
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2022-02-13 12:03:14 -0500

    fixing login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-//const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');
 
 
 router.get('/', (req, res) => {
@@ -118,28 +118,28 @@ router.get('/', (req, res) => {
     }
   });
   
-  // router.put('/:id', (req, res) => {
-  //   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+  router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   
-  //   // pass in req.body instead to only update what's passed through
-  //   User.update(req.body, {
-  //     individualHooks: true,
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   })
-  //     .then(dbUserData => {
-  //       if (!dbUserData) {
-  //         res.status(404).json({ message: 'No user found with this id' });
-  //         return;
-  //       }
-  //       res.json(dbUserData);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //       res.status(500).json(err);
-  //     });
-  // });
+    // pass in req.body instead to only update what's passed through
+    User.update(req.body, {
+      individualHooks: true,
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
   
   router.delete('/:id', (req, res) => {
     User.destroy({
